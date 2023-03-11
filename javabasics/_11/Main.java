ab3e75984b5da0719c65337c2d2e773826653d29
Andrea Fusari <andrea.fusari@hotmail.com>
2023-03-11 18:09:35 +0100

    completato esercizio _11

--- javabasics/_11/Main.java
+++ javabasics/_11/Main.java
@@ -28,6 +28,17 @@ public class Main {
                 orderCost = 2.50;
                 break;
             // Your cases here!
+            case "cheeseburger":
+                orderCalories= 850;
+                orderCost= 7.99;
+                break;
+            case "fries":
+                orderCalories= 250;
+                orderCost= 3.99;
+                break;
+            default:
+                System.out.println("Unknown");
+            break;
         }
 
         System.out.println("Order receipt:" +
@@ -53,8 +64,36 @@ public class Main {
         System.out.println("\nExercise 2 - Prime numbers:");
 
         int inputNumber = 17;
-
         // Your switch statement here
+        switch (inputNumber){
+                 case 2 :
+                 System.out.println("this is the 8th largest prime number under 20");
+                 break;
+                 case 3 :
+                 System.out.println("this is the 7th largest prime number under 20");
+                 break;
+                 case 5 :
+                 System.out.println("this is the 6th largest prime number under 20");
+                 break;
+                 case 7 :
+                 System.out.println("this is the 5th largest prime number under 20");
+                 break;
+                 case 11 :
+                 System.out.println("this is the 4th largest prime number under 20");
+                 break;
+                 case 13 :
+                 System.out.println("this is the 3rd largest prime number under 20");
+                 break;
+                 case 17 :
+                 System.out.println("this is the 2th largest prime number under 20");
+                 break;
+                 case 19 :
+                 System.out.println("this is the largest prime number under 20");
+                 break;
+                 default:
+                 System.out.println("Invalid entry");
+                 break;
+    }
     }
 
     /**
@@ -68,5 +107,25 @@ public class Main {
         System.out.println("\nExercise 3 - Prime  numbers (Bonus)");
 
         // Your switch statement here
-    }
+        int inputNumber = 5;
+
+        switch (inputNumber){
+            case 2 -> System.out.println("this is the 8th largest prime number under 20");
+
+            case 3 -> System.out.println("this is the 7th largest prime number under 20");
+
+            case 5 -> System.out.println("this is the 6th largest prime number under 20");
+
+            case 7 -> System.out.println("this is the 5th largest prime number under 20");
+
+            case 11 -> System.out.println("this is the 4th largest prime number under 20");
+
+            case 13 -> System.out.println("this is the 3rd largest prime number under 20");
+
+            case 17 -> System.out.println("this is the 2th largest prime number under 20");
+
+            case 19 -> System.out.println("this is the largest prime number under 20");
+
+            default->  System.out.println("Invalid entry");
+        }}
 }
