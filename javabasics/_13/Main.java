74ab52c696a6dc61de9f9e89edbf4a32aa0d3498
Andrea Fusari <andrea.fusari@hotmail.com>
2023-03-14 11:09:29 +0100

    completato esercizio _13

--- javabasics/_13/Main.java
+++ javabasics/_13/Main.java
@@ -16,13 +16,25 @@ public class Main {
      * <p>
      * Remove the first 3 items
      * <p>
-     * The print out the first remaining item
+     * Then print out the first remaining item
      */
     public static void exercise1() {
         System.out.println("Exercise 1:");
         ArrayList<Integer> numbers = new ArrayList<>();
-
         //Write your code here
+        numbers.add(37);
+        numbers.add(4);
+        numbers.add(32);
+        numbers.add(67);
+        numbers.add(12);
+        numbers.add(6);
+        numbers.add(16);
+        numbers.add(88);
+        System.out.println(numbers.get(0));
+        numbers.remove(0);
+        numbers.remove(0);
+        numbers.remove(0);
+        System.out.println(numbers.get(0));
     }
 
     /**
@@ -32,14 +44,30 @@ public class Main {
      * <p>
      * And a list of Doubles called 'menuPrices'
      * <p>
-     * The indexes of these two lists correspond to eachother
+     * The indexes of these two lists correspond to each other
      * <p>
      * Create a print statement to output the name and price of each
      * menu item correspond
      */
     public static void exercise2() {
-        System.out.println("\nExercise 1:");
+        System.out.println("\nExercise 2:");
         //Write your code here
+        ArrayList <String> menuItems = new ArrayList<>();
+        ArrayList <Double> menuPrices = new ArrayList<>();
+
+        menuItems.add("Pizza Margherita");
+        menuPrices.add(5.50);
+
+        menuItems.add("Insalata Caprese");
+        menuPrices.add(8.00);
+
+        menuItems.add("Spaghetti alla Carbonara");
+        menuPrices.add(6.99);
+
+        System.out.println(menuItems.get(0)+" "+ menuPrices.get(0)+" $");
+        System.out.println(menuItems.get(1)+" "+ menuPrices.get(1)+" $");
+        System.out.println(menuItems.get(2)+" "+ menuPrices.get(2)+" $");
+
     }
 
     /**
@@ -55,8 +83,9 @@ public class Main {
      */
     public static void exercise3() {
         System.out.println("\nExercise 3:");
-        int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
-
         //Write your code here
+        int[] items = new int[]{5, 10404, -234348, -53, 9, 91, 92, 34534, 12334, 1435};
+        int totalSum = items[0]+items[1]+items[2]+items[3]+items[4]+items[5]+items[6]+items[7]+items[8]+items[9];
+        System.out.println(totalSum);
     }
 }
