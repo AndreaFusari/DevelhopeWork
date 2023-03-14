c0d115fa96290188bcfa373066b107ceb7960242
Andrea Fusari <andrea.fusari@hotmail.com>
2023-03-14 14:49:51 +0100

    completato esercizio _15

--- javabasics/_15/Main.java
+++ javabasics/_15/Main.java
@@ -10,8 +10,8 @@ public class Main {
      * 1a: Edit this while loop to stop once our currentInvestmentValueEuros reaches €100,000
      * <p>
      * 1b: Change the same line, but now instead of currentInvestmentValueEuros reaching €100,000
-     *
-     *     Let's just run it for 5 years OR until it reaches €15,000 use || just like an if!
+     * <p>
+     * Let's just run it for 5 years OR until it reaches €15,000 use || just like an if!
      */
     private static void exercise1() {
         System.out.println("Exercise 1 - Compounding Interest:");
@@ -21,10 +21,11 @@ public class Main {
         double perYearMultiplier = 1 + (averageReturnFromStockMarketIndexPercentage / 100);
         int yearsPast = 0;
         double interestGainedThisYearEuros = 0;
-
-        while (true) {
-            System.out.println("Investment value = €" + currentInvestmentValueEuros + " after " + yearsPast++ + " years" +
-                    ", interest this year €" + interestGainedThisYearEuros);
+        // 1a  while (currentInvestmentValueEuros<=100_000)
+        // 1b while  (currentInvestmentValueEuros<=15_000 || yearsPast <= 5)
+        while (currentInvestmentValueEuros <= 15_000 || yearsPast <= 5) {
+            System.out.println("Investment value = €" + Math.round(currentInvestmentValueEuros) + " after " + yearsPast++ + " years" +
+                    ", interest this year €" + Math.round(interestGainedThisYearEuros));
             double previousYearsInvestment = currentInvestmentValueEuros;
             currentInvestmentValueEuros = currentInvestmentValueEuros * perYearMultiplier;
             interestGainedThisYearEuros = currentInvestmentValueEuros - previousYearsInvestment;
@@ -33,33 +34,41 @@ public class Main {
 
     /**
      * 2: Write a while loop for our trading bot!
-     *
-     *    The bot is going through a financial catastrophe! It's holding 10,000 great british pounds.
-     *
-     *    It wants to sell all of these as soon as possible! Before the state of Britain collapses, but it's a bit greedy.
-     *    It will only sell its pounds when the exchange rate is greater than 1.15 (poundToEuro)
-     *
-     *    On days where it sells, it can only sell 1000 pounds for euros.
-     *
-     *    Write a while loop
-     *
-     *    Use the calculation for exchange rate below
-     *    1 + (Math.random() * 0.2);
-     *
-     *    to recalculate our rate every day (once per loop)
-     *
-     *    Print the number of days it takes for our bot to sell its pounds
+     * <p>
+     * The bot is going through a financial catastrophe! It's holding 10,000 great british pounds.
+     * <p>
+     * It wants to sell all of these as soon as possible! Before the state of Britain collapses, but it's a bit greedy.
+     * It will only sell its pounds when the exchange rate is greater than 1.15 (poundToEuro)
+     * <p>
+     * On days where it sells, it can only sell 1000 pounds for euros.
+     * <p>
+     * Write a while loop
+     * <p>
+     * Use the calculation for exchange rate below
+     * 1 + (Math.random() * 0.2);
+     * <p>
+     * to recalculate our rate every day (once per loop)
+     * <p>
+     * Print the number of days it takes for our bot to sell its pounds
      */
     private static void exercise2() {
         System.out.println("Exercise 2 - Selling the pound:");
-        double poundToEuroExchangeRate = 1 + (Math.random() * 0.2);
         int daysToSell = 0;
         int remainingPoundsToSell = 10_000;
         int maxPoundsToSellPerDay = 1_000;
 
         // Write your while loop here
+        while (remainingPoundsToSell > 0) {
+            daysToSell++;
+            double poundToEuroExchangeRate = 1 + (Math.random() * 0.2);
 
-        System.out.println("It took " + daysToSell + " to exit that cursed economy");
+            if (poundToEuroExchangeRate > 1.15) {
+                remainingPoundsToSell -= maxPoundsToSellPerDay;
+            }
+        }
+
+        System.out.println("It took " + daysToSell + " days to exit that cursed economy");
+
     }
 
     /**
@@ -73,10 +82,10 @@ public class Main {
      * A1:
      *
      * Q2: We have a list of 1000 users to send emails to
-     * A2:
+     * A2:for each
      *
      * Q3: We want to print out the first 80 customer names from our bank database
-     * A3:
+     * A3: fori
      *
      * Q4: We want to read a file 100 lines at a time, without loading the full file into our program
      * A4:
